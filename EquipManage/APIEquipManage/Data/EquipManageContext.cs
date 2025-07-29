116631c0bf8b6aca8590e74c55f4fc98d8a9ec9f
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-07-29 01:21:17 +0100

    configs for connection database

--- EquipManage/APIEquipManage/Data/EquipManageContext.cs
+++ EquipManage/APIEquipManage/Data/EquipManageContext.cs
@@ -7,12 +7,12 @@ namespace APIEquipManage.Data
     public class EquipManageContext : DbContext
     {
         public EquipManageContext(DbContextOptions<EquipManageContext> options) : base(options) { }
-
-        public DbSet<Equipment> Equips { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Reservation> Reservations { get; set; }
-        public DbSet<Status> Statuss { get; set; }
-        public DbSet<StatusOpt> StatusOpts { get; set; }   
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Equipment> Equipment { get; set; }
+        public DbSet<Image> Image { get; set; }
+        public DbSet<Reservation> Reservation { get; set; }
+        public DbSet<Status> Status { get; set; }
+        public DbSet<StatusOpt> StatusOpt { get; set; }   
 
     }
 }
--- EquipManage/APIEquipManage/Program.cs
+++ EquipManage/APIEquipManage/Program.cs
@@ -8,15 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<EquipManageContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
