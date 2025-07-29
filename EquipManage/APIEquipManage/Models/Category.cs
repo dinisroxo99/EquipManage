a77701603d002f07bf6ed9251644b73feb8f89d3
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-07-29 03:07:35 +0100

    change idParent to null

--- EquipManage/APIEquipManage/Models/Category.cs
+++ EquipManage/APIEquipManage/Models/Category.cs
@@ -1,9 +1,11 @@
-﻿namespace APIEquipManage.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIEquipManage.Models
 {
     public class Category
     {
         public required int Id { get; set; }
-        public int IdParent { get; set; }
+        public int? IdParent { get; set; }
         public required string Name { get; set; }
     }
 }
