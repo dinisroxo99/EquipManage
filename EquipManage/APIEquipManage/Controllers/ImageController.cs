5857d62a1cc7b974b4f920255548d02ba56b3bff
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-08-01 00:12:24 +0100

    update ImageController

--- EquipManage/APIEquipManage/Controllers/ImageController.cs
+++ EquipManage/APIEquipManage/Controllers/ImageController.cs
@@ -4,65 +4,63 @@ using APIEquipManage.Extensions;
 using APIEquipManage.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Web.Http.Results;
 
-namespace APIEquipManage.Controllers
+
+[ApiController]
+[Route("api/equioment/{id}")]
+public class ImageController : ControllerBase
 {
-    [ApiController]
-    [Route("api/equioment/{id}")]
-    public class ImageController : ControllerBase
+    private readonly EquipManageContext _equipManageContext;
+
+    public ImageController(EquipManageContext equipManageContext)
     {
-        private readonly EquipManageContext _equipManageContext;
-
-        public ImageController(EquipManageContext equipManageContext)
-        {
-            _equipManageContext = equipManageContext;
-        }
-
-        [HttpPost("image")]
-        public async Task<IActionResult> NewImage([FromRoute] int id, [FromBody] UpdateImagesDTO updateImages)
-        {
-            try
-            {
-                var equipment = await _equipManageContext.Equipment.IncludeImg().FirstAsync(x => x.Id == id);
-                var addedImg = new List<Image>();
-                foreach (var imgPath in updateImages.ImageUrl)
-                {
-                    var img = new Image { ImagePath = imgPath , IdEquipment = id};
-                    _equipManageContext.Image.Add(img);
-                    addedImg.Add(img);
-                }
-                await _equipManageContext.SaveChangesAsync();
-                return Ok(addedImg);
-            }
-            catch (Exception e)
-            {
-
-                return BadRequest(e.Message);
-            }
-        }
-        [HttpDelete("image/{idImg}")]
-        public async Task<IActionResult> DeleteImage([FromRoute] int id, int idImg)
-        {
-            try
-            {
-                var img = await _equipManageContext.Image.FindAsync(idImg);
-                if (img != null && img.IdEquipment == id)
-                {
-                    _equipManageContext.Image.Remove(img);
-                    await _equipManageContext.SaveChangesAsync();
-                    return Ok(img);
-                }
-                return 
-                
-            }
-            catch (Exception e)
-            {
-
-                return BadRequest(e.Message);
-            }
-        }
-
-
+        _equipManageContext = equipManageContext;
     }
+
+    [HttpPost("image")]
+    public async Task<IActionResult> NewImage([FromRoute] int id, [FromBody] UpdateImagesDTO updateImages)
+    {
+        try
+        {
+            var equipment = await _equipManageContext.Equipment.IncludeImg().FirstAsync(x => x.Id == id);
+            var addedImg = new List<Image>();
+            foreach (var imgPath in updateImages.ImageUrl)
+            {
+                var img = new Image { ImagePath = imgPath , IdEquipment = id};
+                await _equipManageContext.Image.AddAsync(img);
+                addedImg.Add(img);
+            }
+            await _equipManageContext.SaveChangesAsync();
+            return Ok(addedImg);
+        }
+        catch (Exception e)
+        {
+
+            return BadRequest(e.Message);
+        }
+    }
+    [HttpDelete("image/{idImg}")]
+    public async Task<IActionResult> DeleteImage([FromRoute] int id, [FromRoute] int idImg)
+    {
+        try
+        {
+            var img = await _equipManageContext.Image.FindAsync(idImg);
+            if (img != null && img.IdEquipment == id)
+            {
+                _equipManageContext.Image.Remove(img);
+                await _equipManageContext.SaveChangesAsync();
+                return Ok(img);
+            }
+            return NotFound();
+                
+        }
+        catch (Exception e)
+        {
+
+            return BadRequest(e.Message);
+        }
+    }
+
+
 }
+
