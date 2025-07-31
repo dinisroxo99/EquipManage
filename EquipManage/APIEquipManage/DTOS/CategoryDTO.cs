6e15b850c7cf9973ea97634e35beedd30eacc066
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-08-01 00:13:52 +0100

    create SubCategoryDTO CategoryDTO CategoryConflictDTO

---
+++ EquipManage/APIEquipManage/DTOS/CategoryDTO.cs
@@ -0,0 +1,22 @@
+﻿using APIEquipManage.Models;
+
+namespace APIEquipManage.DTOS
+{
+    public class SubCategoryDTO
+    {
+        public List<string> SubCategory {  get; set; } = new List<string>();
+    }
+
+    public class CategoryDTO
+    {
+        public List<string> Category { get; set; } = new List<string>();
+    }
+    public class CategoryConflictDTO
+    {
+        public string Message { get; set; } = "";
+        public List<Equipment> Equipamentos { get; set; } = new List<Equipment>();
+        public List<Category> Subcategorias { get; set; } = new List<Category>();
+    }
+
+
+}
