687b2eabea78d7807c1e1549e1b9a45690d0f83e
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-08-01 00:14:11 +0100

    update model of Category

--- EquipManage/APIEquipManage/Models/Category.cs
+++ EquipManage/APIEquipManage/Models/Category.cs
@@ -4,8 +4,10 @@ namespace APIEquipManage.Models
 {
     public class Category
     {
-        public required int Id { get; set; }
+        public int Id { get; set; }
         public int? IdParent { get; set; }
         public required string Name { get; set; }
+
+        public Category? Parent { get; set; }
     }
 }
