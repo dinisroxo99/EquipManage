893fca80e54273ca8420f2ee967560bfa9e2ed35
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-08-01 21:42:38 +0100

    add mor subclass on equipmentDTO

--- EquipManage/APIEquipManage/DTOS/EquipmentpDTO.cs
+++ EquipManage/APIEquipManage/DTOS/EquipmentpDTO.cs
@@ -2,10 +2,36 @@
 {
     public class UpdateEquipmentDTO
     {
+        public  string? Name { get; set; }
+        public  string? Model { get; set; }
+        public  string? Description { get; set; }
+        public  int? StatusId { get; set; }
+        public  int? CategoryId { get; set; }
+    }
+
+    public class NewEquipmentDTO
+    {
+        public required string Name { get; set; }
+        public required string Model { get; set; }
+        public string? Description { get; set; }
+        public required int StatusId { get; set; }
+        public required int CategoryId { get; set; }
+    }
+
+    public class GetEquipmentDTO
+    {
+        public int Code { get; set; }
+        public required string Name { get; set; }
+        public required string Model { get; set; }
+    }
+
+    public class GetDetailedEquipmentDTO
+    {
+        public int Code { get; set; }
         public required string Name { get; set; }
         public required string Model { get; set; }
         public required string Description { get; set; }
-        public required int StatusId { get; set; }
-        public required int CategoryId { get; set; }
+        public required string Status { get; set; }
+        public required string Category { get; set; }
     }
 }
