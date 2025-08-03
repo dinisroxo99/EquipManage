8c88f706d2f7488cb114dae156a33665a6357d58
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-08-03 17:41:54 +0100

    create DTO for reservation

---
+++ EquipManage/APIEquipManage/DTOS/ReservationDTO.cs
@@ -0,0 +1,21 @@
+﻿using APIEquipManage.Models;
+
+namespace APIEquipManage.DTOS
+{
+    public class GetReservationDTO
+    {
+        public required int Code { get; set; }
+        public required GetEquipmentDTO Equipment { get; set; }
+        public required DateTime Start { get; set; }
+        public required DateTime End { get; set; }
+    }
+
+    public class GetDetailedReservationDTO
+    {
+        public required GetDetailedEquipmentDTO Equipment { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime Start { get; set; }
+        public required DateTime End { get; set; }
+    }
+
+}
