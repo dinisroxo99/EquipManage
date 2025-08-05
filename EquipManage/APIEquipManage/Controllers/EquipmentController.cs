a1746fa3e78cb90530631cacd56be264e8b2c193
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-08-05 01:51:50 +0100

    otimize equipmentcontroller and add pagelist to endpoins with get

--- EquipManage/APIEquipManage/Controllers/EquipmentController.cs
+++ EquipManage/APIEquipManage/Controllers/EquipmentController.cs
@@ -1,247 +1,272 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using APIEquipManage.Data;
-using APIEquipManage.Models;
-using System;
-using Microsoft.IdentityModel.Tokens;
-using APIEquipManage.Extensions;
+﻿using APIEquipManage.Data;
 using APIEquipManage.DTOS;
+using APIEquipManage.Extensions;
+using APIEquipManage.Helpers;
+using APIEquipManage.Models;
+using Azure;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
 
-[ApiController]
-[Route("api/equipment")]
-public class EquipmentController : ControllerBase
+namespace APIEquipManage.Controllers
 {
-    private readonly EquipManageContext _equipManageContext;
+    [ApiController]
+    [Route("api/equipment")]
+    public class EquipmentController(EquipManageContext equipManageContext) : ControllerBase
+    {
+        private readonly EquipManageContext _equipManageContext = equipManageContext;
 
-    public EquipmentController(EquipManageContext equipManageContext)
-    {
-        _equipManageContext = equipManageContext;
-    }
+        [HttpGet]
+        public async Task<IActionResult> GetEquipment([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            if (page < 1 || pageSize <1 || pageSize > 100){ return BadRequest("Page must be ≥ 1 and pageSize must be between 1 and 100."); }
+            try
+            {
+                
+                var equipments = _equipManageContext.Equipment.Include(e => e.StatusOpt).AsNoTracking();
 
-    [HttpGet]
-    public async Task<IActionResult> GetEquipment()
-    {
-        try
-        {
-            var equipments = await _equipManageContext.Equipment.Include(e => e.StatusOpt).ToListAsync();
-            if (equipments.Count == 0)
-            {
-                return NoContent();
-            }
-            List<GetEquipmentDTO> response = new List<GetEquipmentDTO>();
-            foreach (var equip in equipments)
-            {
-                response.Add(new GetEquipmentDTO() { Code = equip.Id, Name = equip.Name, Model = equip.Model, Status=equip.StatusOpt.Name });
-            }
-            return Ok(response);
-        }
-        catch(Exception e)
-        {
-            return BadRequest(e.Message);
-        }
-        
-   
-        
-    }
-    [HttpGet("search")]
-    public async Task<IActionResult> GetEquiomentByName([FromQuery] string name)
-    {
-        try
-        {
-            var equipment = await _equipManageContext.Equipment.Include(e => e.StatusOpt).AsNoTracking().Where(x => x.Name.Contains(name)).ToListAsync();
-            if (equipment == null)
-            {
-                return NoContent();
-            }
-            List<GetEquipmentDTO> response = new List<GetEquipmentDTO>();
-            foreach (var equip in equipment) 
-            { 
-                response.Add(new GetEquipmentDTO()
+                var pagedEquipments = await PaginatedList<Equipment>.CreateAsync(equipments, page, pageSize);
+                if (pagedEquipments.Items.Count < 1)
                 {
-                    Code = equip.Id,
-                    Name = equip.Name,
-                    Model = equip.Model,
-                    Status = equip.StatusOpt.Name
-                }); 
-            }
-            return Ok(response);
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e.Message);
-        }
-    }
-    [HttpGet("avaliable")]
-    public async Task<IActionResult> GetAvaliableEquipment()
-    {
-        try
-        {
-            var equipments = await _equipManageContext.Equipment.AsNoTracking()
-                                                                .Where(x => x.IdStatus == 6)
-                                                                .ToListAsync();
-
-            if (equipments.Count < 1) { return NoContent(); }
-            List<GetEquipmentDTO> response = new List<GetEquipmentDTO>();
-            foreach (var equip in equipments)
-            {
-                var equipDTO = new GetEquipmentDTO() { Code = equip.Id, Name = equip.Name, Model = equip.Model };
-                response.Add(equipDTO);
-            }
-            return Ok(response);
-        }
-        catch (Exception e)
-        {
-
-            return BadRequest(e.Message);
-        }
-    }
-    [HttpGet("deleted")]
-    public async Task<IActionResult> GetDeletedEquipment()
-    {
-        try
-        {
-            var equipments = await _equipManageContext.Equipment.AsNoTracking()
-                                                                .Where(x => x.IdStatus == 5)
-                                                                .ToListAsync();
-
-            if (equipments.Count < 1) { return NoContent(); }
-            List<GetEquipmentDTO> response = new List<GetEquipmentDTO>();
-            foreach (var equip in equipments)
-            {
-                var equipDTO = new GetEquipmentDTO() { Code = equip.Id, Name = equip.Name, Model = equip.Model };
-                response.Add(equipDTO);
-            }
-            return Ok(response);
-        }
-        catch (Exception e)
-        {
-
-            return BadRequest(e.Message);
-        }
-    }
-    [HttpPost]
-    public async Task<IActionResult> NewEquipment([FromBody] NewEquipmentDTO equipment)
-    {
-        try
-        {
-            var newEquipment = new Equipment() {
-                Name=equipment.Name, 
-                Model=equipment.Model, 
-                Description=equipment.Description,
-                IdStatus=equipment.StatusId,
-                IdCategory=equipment.CategoryId,
-                CreatedAt=DateTime.UtcNow
-            };
-            _equipManageContext.Equipment.Add(newEquipment);
-            await _equipManageContext.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetEquipment), new { name = newEquipment.Name }, newEquipment);
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e.Message);
-        }
-    }
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteEquipment(int id)
-    {
-        var equipment = await _equipManageContext.Equipment.FindAsync(id);
-        if (equipment == null)
-        {
-            return Forbid();
-        }
-        var canceledReservations = new List<Reservation>(); 
-        var reservation = await _equipManageContext.Reservation.Where(r => r.IdEquipment == id).ToListAsync();
-        if (reservation.Count > 0)
-        {
-            foreach (var r in reservation)
-            {
-                if (r.EndDate > DateTime.UtcNow)
-                {
-                    r.CanceledAt = DateTime.UtcNow;
-                    canceledReservations.Add(r);
+                    return NoContent();
                 }
+
+                var equipmentsDTO = pagedEquipments.Items.Select(equip => new EquipmentDTO() { Code = equip.Id, Name = equip.Name, Model = equip.Model, Status = equip.StatusOpt?.Name }).ToList();
+                var response = new
+                {
+                    pagedEquipments.HasPreviousPage,
+                    pagedEquipments.PageIndex,
+                    pagedEquipments.HasNextPage,
+                    pagedEquipments.TotalPages,
+                    Equipments = equipmentsDTO
+                };
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
             }
         }
-        var deletedStatus = await _equipManageContext.StatusOpt.FirstOrDefaultAsync(x => x.Name == "Deleted");
-        if (deletedStatus == null)
+        [HttpGet("search")]
+        public async Task<IActionResult> GetEquiomentByName([FromQuery] string name, [FromQuery] int page = 1, [FromQuery] int pageSize = 10)
         {
-            return BadRequest(new { message = "Can't find the option to Delete" });
-        }
-        equipment.IdStatus = deletedStatus.Id;
-        var response = new DeletedEquipmentDTO()
-        {
-            Code = equipment.Id,
-            Name = equipment.Name,
-            Model = equipment.Model,
-            Status = equipment.StatusOpt.Name,
-            CanceledReservation = canceledReservations
-        };
-        await _equipManageContext.SaveChangesAsync();
-        return Ok(response);
-    }
+            if (page < 1 || pageSize < 1 || pageSize > 100) { return BadRequest("Page must be ≥ 1 and pageSize must be between 1 and 100."); }
+            try
+            {
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateEquipment(int id, [FromBody] UpdateEquipmentDTO updateFields)
-    {
-        try
+                var equipments = _equipManageContext.Equipment.Include(e => e.StatusOpt).AsNoTracking().Where(x => x.Name.Contains(name));
+                var pagedEquipments = await PaginatedList<Equipment>.CreateAsync(equipments, page, pageSize);
+                if (pagedEquipments.Items.Count < 1)
+                {
+                    return NoContent();
+                }
+
+                var equipmentsDTO = pagedEquipments.Items.Select(equip => new EquipmentDTO() { Code = equip.Id, Name = equip.Name, Model = equip.Model, Status = equip.StatusOpt?.Name }).ToList();
+                var response = new
+                {
+                    pagedEquipments.HasPreviousPage,
+                    pagedEquipments.PageIndex,
+                    pagedEquipments.HasNextPage,
+                    pagedEquipments.TotalPages,
+                    Equipments = equipmentsDTO
+                };
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpGet("avaliable")]
+        public async Task<IActionResult> GetAvaliableEquipment([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                var equipments = _equipManageContext.Equipment.AsNoTracking().Where(x => x.IdStatus == 6);
+
+                var pagedEquipments = await PaginatedList<Equipment>.CreateAsync(equipments, page, pageSize);
+                if (pagedEquipments.Items.Count < 1)
+                {
+                    return NoContent();
+                }
+
+                var equipmentsDTO = pagedEquipments.Items.Select(equip => new EquipmentDTO() { Code = equip.Id, Name = equip.Name, Model = equip.Model, Status = equip.StatusOpt?.Name }).ToList();
+                var response = new
+                {
+                    pagedEquipments.HasPreviousPage,
+                    pagedEquipments.PageIndex,
+                    pagedEquipments.HasNextPage,
+                    pagedEquipments.TotalPages,
+                    Equipments = equipmentsDTO
+                };
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpGet("deleted")]
+        public async Task<IActionResult> GetDeletedEquipment([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                var equipments = _equipManageContext.Equipment.AsNoTracking()
+                                                                    .Where(x => x.IdStatus == 5);
+
+                var pagedEquipments = await PaginatedList<Equipment>.CreateAsync(equipments, page, pageSize);
+                if (pagedEquipments.Items.Count < 1)
+                {
+                    return NoContent();
+                }
+
+                var equipmentsDTO = pagedEquipments.Items.Select(equip => new EquipmentDTO() { Code = equip.Id, Name = equip.Name, Model = equip.Model, Status = equip.StatusOpt?.Name }).ToList();
+                var response = new
+                {
+                    pagedEquipments.HasPreviousPage,
+                    pagedEquipments.PageIndex,
+                    pagedEquipments.HasNextPage,
+                    pagedEquipments.TotalPages,
+                    Equipments = equipmentsDTO
+                };
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> NewEquipment([FromBody] NewEquipmentDTO equipment)
+        {
+            try
+            {
+                var newEquipment = new Equipment()
+                {
+                    Name = equipment.Name,
+                    Model = equipment.Model,
+                    Description = equipment.Description,
+                    IdStatus = equipment.StatusId,
+                    IdCategory = equipment.CategoryId,
+                    CreatedAt = DateTime.UtcNow
+                };
+                _equipManageContext.Equipment.Add(newEquipment);
+                await _equipManageContext.SaveChangesAsync();
+                return CreatedAtAction(nameof(GetEquipment), new { name = newEquipment.Name }, newEquipment);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEquipment(int id)
         {
             var equipment = await _equipManageContext.Equipment.FindAsync(id);
             if (equipment == null)
-                return NotFound();
-            if (equipment.IdStatus == 6 && updateFields.StatusId !=6)
             {
-                equipment.DeletedAt = null;
+                return Forbid();
             }
-            equipment.IdStatus = updateFields.StatusId;
-            equipment.IdCategory = updateFields.CategoryId;
-            equipment.Name = updateFields.Name;
-            equipment.Model = updateFields.Model;
-            equipment.Description = updateFields.Description;
-            
-            await _equipManageContext.SaveChangesAsync();
-
-            return Ok("Update Successfully");
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e.Message);
-        }
-    }
-    [HttpPost("{idEquipment}/reservation")]
-    public async Task<IActionResult> NewReservation([FromRoute] int idEquipment, [FromBody] NewReservationDTO newReservationDTO)
-    {
-        if (newReservationDTO.Start < DateTime.UtcNow)
-        {
-            return BadRequest("data-hora mal");
-        }
-        try
-        {
-            var equipmentDependence = await _equipManageContext.Reservation.Where(x => x.IdEquipment == idEquipment).ToListAsync();
-
-            foreach (var existing in equipmentDependence)
+            var canceledReservations = new List<Reservation>();
+            var reservation = await _equipManageContext.Reservation.Where(r => r.IdEquipment == id).ToListAsync();
+            if (reservation.Count > 0)
             {
-                bool isOverlapping =
-                newReservationDTO.Start < existing.EndDate &&
-                newReservationDTO.End > existing.StartDate;
-                if (isOverlapping) { return BadRequest("There is already a reservation in that time slot."); }
+                foreach (var r in reservation)
+                {
+                    if (r.EndDate > DateTime.UtcNow)
+                    {
+                        r.CanceledAt = DateTime.UtcNow;
+                        canceledReservations.Add(r);
+                    }
+                }
             }
-
-
-            var newReservation = new Reservation() {
-                IdEquipment = idEquipment,
-                CreatedAt = DateTime.UtcNow,
-                StartDate = newReservationDTO.Start,
-                EndDate = newReservationDTO.End
+            var deletedStatus = await _equipManageContext.StatusOpt.FirstOrDefaultAsync(x => x.Name == "Deleted");
+            if (deletedStatus == null)
+            {
+                return BadRequest(new { message = "Can't find the option to Delete" });
+            }
+            equipment.IdStatus = deletedStatus.Id;
+            var response = new DeletedEquipmentDTO()
+            {
+                Code = equipment.Id,
+                Name = equipment.Name,
+                Model = equipment.Model,
+                Status = equipment.StatusOpt.Name,
+                CanceledReservation = canceledReservations
             };
-            _equipManageContext.Reservation.Add(newReservation);
             await _equipManageContext.SaveChangesAsync();
-
-            return Ok("Created Successfully");
+            return Ok(response);
         }
-        catch (Exception e)
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEquipment(int id, [FromBody] UpdateEquipmentDTO updateFields)
         {
-            return BadRequest(e.Message);
+            try
+            {
+                var equipment = await _equipManageContext.Equipment.FindAsync(id);
+                if (equipment == null)
+                    return NotFound();
+                if (equipment.IdStatus == 6 && updateFields.StatusId != 6)
+                {
+                    equipment.DeletedAt = null;
+                }
+                equipment.IdStatus = updateFields.StatusId;
+                equipment.IdCategory = updateFields.CategoryId;
+                equipment.Name = updateFields.Name;
+                equipment.Model = updateFields.Model;
+                equipment.Description = updateFields.Description;
+
+                await _equipManageContext.SaveChangesAsync();
+
+                return Ok($"Equipment \"{equipment.Name}\" as ben updated successfully");
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPost("{idEquipment}/reservation")]
+        public async Task<IActionResult> NewReservation([FromRoute] int idEquipment, [FromBody] NewReservationDTO newReservationDTO)
+        {
+            if (newReservationDTO.Start < DateTime.UtcNow){ return BadRequest("The reservation start date and time cannot be in the past."); }
+            if (newReservationDTO.End < newReservationDTO.Start) { return BadRequest("The reservation end date must be after the start date."); }
+            try
+            {
+                var equipmentDependence = await _equipManageContext.Reservation.Where(x => x.IdEquipment == idEquipment).ToListAsync();
+
+                bool hasOverLap = equipmentDependence.Any(existing =>
+                    newReservationDTO.Start < existing.EndDate &&
+                    newReservationDTO.End > existing.StartDate);
+
+                if (hasOverLap)
+                {
+                    return BadRequest("There is already a reservation in that time slot.");
+                }
+
+
+                var newReservation = new Reservation()
+                {
+                    IdEquipment = idEquipment,
+                    CreatedAt = DateTime.UtcNow,
+                    StartDate = newReservationDTO.Start,
+                    EndDate = newReservationDTO.End
+                };
+                _equipManageContext.Reservation.Add(newReservation);
+                await _equipManageContext.SaveChangesAsync();
+
+                //return Ok("Created Successfully");
+                return CreatedAtRoute("GetReservationById", new { id = newReservation.Id }, newReservation);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
-
--- EquipManage/APIEquipManage/DTOS/EquipmentpDTO.cs
+++ EquipManage/APIEquipManage/DTOS/EquipmentpDTO.cs
@@ -5,11 +5,11 @@ namespace APIEquipManage.DTOS
 {
     public class UpdateEquipmentDTO
     {
-        public  string Name { get; set; }
-        public  string Model { get; set; }
-        public  string Description { get; set; }
-        public  int StatusId { get; set; }
-        public  int CategoryId { get; set; }
+        public  required string Name { get; set; }
+        public required string Model { get; set; }
+        public required string Description { get; set; }
+        public required int StatusId { get; set; }
+        public required int CategoryId { get; set; }
     }
 
     public class NewEquipmentDTO
@@ -21,7 +21,7 @@ namespace APIEquipManage.DTOS
         public required int CategoryId { get; set; }
     }
 
-    public class GetEquipmentDTO
+    public class EquipmentDTO
     {
         public int Code { get; set; }
         public required string Name { get; set; }
@@ -30,7 +30,7 @@ namespace APIEquipManage.DTOS
         public string? Status { get; set; }
     }
 
-    public class GetDetailedEquipmentDTO
+    public class DetailedEquipmentDTO
     {
         public int Code { get; set; }
         public required string Name { get; set; }
@@ -48,5 +48,12 @@ namespace APIEquipManage.DTOS
         public required string Status { get; set; }
         public DateTime DeletedAt { get; set; } = DateTime.UtcNow;
         public List<Reservation> CanceledReservation = new List<Reservation>();
+    }
+
+    public class EquipmentCategoryDTO
+    {
+        public int Code { get; set; }
+        public required string Name { get; set; }
+        public required string Category { get; set; }
     }
 }
