0c99b5b456ce060dc0b41a107edae7f7924102b7
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-08-06 01:54:30 +0100

    add cors to allow localhost

--- EquipManage/APIEquipManage/Program.cs
+++ EquipManage/APIEquipManage/Program.cs
@@ -8,25 +8,35 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<EquipManageContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllLocalhost", policy =>
+    {
+        policy.AllowAnyOrigin()   // aceita requests de qualquer origem
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
 app.UseHttpsRedirection();
+
+// CORS deve estar antes do UseAuthorization e MapControllers
+app.UseCors("AllowAllLocalhost");
 
 app.UseAuthorization();
 
