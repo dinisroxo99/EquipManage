4e6d387b884e7a4ce8c69772af66082642218e16
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-08-06 01:09:48 +0100

    add variable API_BASE_URL

--- EquipManage/BlazorEquipManage/Program.cs
+++ EquipManage/BlazorEquipManage/Program.cs
@@ -1,10 +1,17 @@
 using BlazorEquipManage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var baseUrl = builder.Configuration["ApiSettings:BaseUrl"]
+    ?? throw new InvalidOperationException("API base URL is not configured.");
+
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri(baseUrl) });
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
