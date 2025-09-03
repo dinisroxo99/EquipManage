1993b18c1f7d2e4d0a8afe7c20b2638b8adbf9db
dinisroxo99 <80784809+dinisroxo99@users.noreply.github.com>
2025-09-03 10:53:30 +0100

    update category model

---
+++ EquipManage/BlazorEquipManage/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorEquipManage.Models
+{
+    public class Category
+    {
+        public required int Code { get; set; }
+        public int? Parent {  get; set; }
+        public required string Name { get; set; }
+    }
+}
